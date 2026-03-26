46b2d8489d99e4fba6c579373fcc64ebc16bfb4f
Roshann78 <grd.roshan78@gmail.com>
2026-03-26 21:24:53 +0530

    callback hell

---
+++ Filereading/index2.js
@@ -0,0 +1,14 @@
+const fs=require('fs');
+
+fs.readFile('fl1.txt','utf-8',(err,data)=>{
+    fs.readFile(`${data}.txt`,'utf-8',(err,data1)=>{
+        console.log(data1);
+        fs.readFile('fl3.txt','utf-8',(err,data2)=>{
+            console.log(data2);
+            fs.writeFile('fl4.txt',`${data1}\n${data2}`,'utf-8',err=>{
+                console.log('File Written');
+            });
+        });
+    });
+});
+console.log('reading');
