8243ea2638424b0f5465ba5dd4cd06f01a3f2cc4
Roshann78 <grd.roshan78@gmail.com>
2026-03-28 00:51:33 +0530

    routing

---
+++ Intro_to_Node_and_NPM/Routing/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const PORT=8000;
+const url=require('url');
+
+const server = http.createServer((req,res)=>{
+    const pathname=req.url;
+    if(pathname==='/' || pathname==='/overview'){
+        res.end('Schrodinger Welcomes You to the OVERVIEW');
+    }
+    else if(pathname==='/realname'){
+        res.end('The real name of Schrodinger is Roshan');
+    }
+    else{
+        res.writeHead(404,{
+            'content-type':'text/html',
+            'my-own-header':'koi baat nahi chalta hai'
+        });
+        res.end('<h1>Hello Schrodinger at but no one found</h1>');
+    }
+    
+});
+
+
+server.listen(PORT,'127.0.0.1',()=>{
+    console.log(`Listening to server on port ${PORT}`);
+});
