7ac9ad5d6cff802a4bc8bdc86cfa7a03e549afd5
Roshann78 <grd.roshan78@gmail.com>
2026-03-30 19:43:54 +0530

    simple api trying

---
+++ Intro_to_Node_and_NPM/SimpleApi/index.js
@@ -0,0 +1,38 @@
+const http=require('http');
+const url=require('url');
+const fs=require('fs');
+const PORT=8000;
+
+const data=fs.readFileSync('data.json','utf-8');
+const dataObj=JSON.parse(data);
+
+const server=http.createServer((req,res)=>{
+    const pathName=req.url;
+    if(pathName==='/' || pathName==='/overview'){
+        res.end('WELCOME TO SCHRODINGERS DEN');
+    }
+    else if(pathName==='/realname'){
+        res.writeHead(200,{
+            'content-type':'text/html',
+            'my-own-header':'koi baat nahi chalta hai dunya hai'
+        });
+        res.end('<h1>My real name is Schrodinger</h1>');
+    }
+    else if(pathname==='/detailsapi'){
+        res.writeHead(200,{
+            'content-type':'',
+            'my-own-header':'koi baat nahi chalta hai dunya hai'
+        });
+    }
+    else{
+        res.writeHead(404,{
+            'content-type':'text/html',
+            'my-own-header':'koi baat nahi chalta hai dunya hai'
+        });
+        res.end('<h1>Schrodinger is alone at the server</h1>');
+    }
+});
+
+server.listen(PORT,()=>{
+    console.log(`Schrodinger listening at port ${PORT}`);
+});
